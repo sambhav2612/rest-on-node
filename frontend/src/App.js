a40499c76aedc82e52496f7eed3374281bed2aca
Sambhav Jain <sambhavjain2612@gmail.com>
2018-02-24 01:30:43 +0530

    add code for connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,4 +21,55 @@ class App extends Component {
   }
 }
 
+class Products extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleproductTitle = this.handleproductTitle.bind(this);
+    this.handleproductPrice = this.handleproductPrice.bind(this);
+    this.handleproductInStock = this.handleproductInStock.bind(this);
+    this.handleproductQuantity = this.handleproductQuantity.bind(this);
+    this.handleproductImage = this.handleproductImage.bind(this);
+
+    this.state = {
+      productTitle: "",
+      productPrice: 0,
+      productInStock: true,
+      productQuantity: 0,
+      productImage: null
+    };
+/*
+    handleproductTitle(event) {
+      this.setState({ productTitle: event.target.value });
+    };
+
+    handleproductPrice(event) {
+      this.setState({ productPrice: event.target.value });
+    };
+
+    handleproductInStock(event) {
+      this.setState({ productInStock: event.target.value });
+    };
+
+    handleproductQuantity(event) {
+      this.setState({ productQuantity: event.target.value });
+    };
+
+    handleproductImage(event) {
+      this.setState({ productImage: event.target.value });
+    };
+*/
+  }
+  render() {
+    return (
+      <p className="App-intro">
+          <button>POST</button>
+          <button>GET</button>
+          <button>PUT</button>
+          <button>DELETE</button>
+        </p>
+    )
+  }
+}
+
 export default App;
